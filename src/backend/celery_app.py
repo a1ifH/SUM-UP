794ffbb1802ec48dc23bbd7c50398e1ae60db253
Muhammad Zubair Asif <muhammad.asif2@mail.dcu.ie>
2023-04-18 01:17:30 +0000

    recognize audio task function implemented

--- src/backend/celery_app.py
+++ src/backend/celery_app.py
@@ -44,3 +44,45 @@ def get_job(job_id, capp):
 
 
 celery_app = create_celery_app(main_app)
+
+
+@celery_app.task(name="recognize_audio")
+def recognize_audio(file):
+    nlp = spacy.load("en_core_web_sm")
+    print(f"test: {file}")
+    logging.debug(f"test: {file}")
+    r = sr.Recognizer()
+
+    with sr.AudioFile(file) as source:
+        audio = r.record(source, duration=30)
+        r.callback = lambda recognizer, audio: print(f"Recognizing audio...")
+        text = " "
+        try:
+            data = r.recognize_google(audio,show_all=False, language='en-US')
+            text += " " + data
+        except sr.UnknownValueError:
+            print("Could not understand audio")
+        chunk_no = 0
+        prev_text = ""
+        curr_text = ""
+        while True:
+            chunk_no += 1
+            audio = r.record(source, duration=5)
+            try:
+                data = r.recognize_google(audio, show_all=False, language='en-US')
+                text += " " + data
+
+                current_task.update_state(state="PROGRESS", meta={"text": text})
+            except sr.UnknownValueError:
+                print("Could not understand audio")
+
+            if len(audio.frame_data) == 0 or r.energy_threshold == 0:
+                logging.debug(text)
+                doc = nlp(text)
+                text_with_punct = ""
+                for i, token in enumerate(doc):
+                    if i > 0 and token.is_alpha and token.is_upper:
+                        text_with_punct += ". "
+                    text_with_punct += token.text_with_ws
+                print(text_with_punct)
+                return text_with_punct
