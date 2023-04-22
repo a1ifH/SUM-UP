df358c3bc69be9b9f103ad2cfd8b37966160fce5
Muhammad Zubair Asif <muhammad.asif2@mail.dcu.ie>
2023-04-22 12:34:42 +0000

    added Signup.js again,
    Create a form for the user to signup taking in their name, email & password. After success the user will be redirected to the dashboard. It uses Firebase Authentication service.

---
+++ src/frontend/src/components/Signup/Signup.js
@@ -0,0 +1,98 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+
+import InputControl from "../InputControl/InputControl";
+import { auth } from "../../firebase";
+
+import styles from "./Signup.module.css";
+
+function Signup() {
+  const navigate = useNavigate();
+  const [values, setValues] = useState({
+    name: "",
+    email: "",
+    pass: "",
+  });
+  const [errorMsg, setErrorMsg] = useState("");
+  const [submitButtonDisabled, setSubmitButtonDisabled] = useState(false);
+
+  const handleSubmission = () => {
+    if (!values.name || !values.email || !values.pass) {
+      setErrorMsg("Fill all fields");
+      return;
+    }
+    setErrorMsg("");
+
+    setSubmitButtonDisabled(true);
+    createUserWithEmailAndPassword(auth, values.email, values.pass)
+      .then(async (res) => {
+        setSubmitButtonDisabled(false);
+        const user = res.user;
+        await updateProfile(user, {
+          displayName: values.name,
+        });
+        window.location.href = "/dashboard";
+        // navigate("/");
+      })
+      .catch((err) => {
+        setSubmitButtonDisabled(false);
+        setErrorMsg(err.message);
+      });
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.headSection}>
+        <div>
+          <img src={process.env.PUBLIC_URL + `/logo.png`} alt="Logo" />
+        </div>
+        <div>
+          <h1 className="text-white">SUM-UP</h1>
+        </div>
+        <div></div>
+      </div>
+      <div className={styles.innerBox}>
+        <h1 className={"fw-bold"}>Signup</h1>
+
+        <InputControl
+          label="Name*"
+          placeholder="Enter your name"
+          onChange={(event) =>
+            setValues((prev) => ({ ...prev, name: event.target.value }))
+          }
+        />
+        <InputControl
+          label="Email*"
+          placeholder="Enter email address"
+          onChange={(event) =>
+            setValues((prev) => ({ ...prev, email: event.target.value }))
+          }
+        />
+        <InputControl
+          label="Password*"
+          placeholder="Enter password"
+          type="password"
+          onChange={(event) =>
+            setValues((prev) => ({ ...prev, pass: event.target.value }))
+          }
+        />
+
+        <div className={styles.footer}>
+          <b className={styles.error}>{errorMsg}</b>
+          <button onClick={handleSubmission} disabled={submitButtonDisabled}>
+            Signup
+          </button>
+          <p>
+            Already have an account?{" "}
+            <span>
+              <Link to="/">Login</Link>
+            </span>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Signup;
