925eb347db1c18584904fce19ced447faebd5e55
Muhammad Zubair Asif <muhammad.asif2@mail.dcu.ie>
2023-04-22 12:12:08 +0000

    Login.js added,
    React component named login that renders a login form with input fields, a submit button, and a link to the sign-up page, as well as a logo and a header.

---
+++ src/frontend/src/components/Login/Login.js
@@ -0,0 +1,89 @@
+import React, { useContext, useState } from "react";
+import { Link } from "react-router-dom";
+import { signInWithEmailAndPassword } from "firebase/auth";
+
+import InputControl from "../InputControl/InputControl";
+import { auth } from "../../firebase";
+
+import styles from "./Login.module.css";
+import { AppContext } from "../../App";
+
+function Login() {
+  const { setFile, setGeneratedText, setSummarizedText } =
+    useContext(AppContext);
+  const [values, setValues] = useState({
+    email: "",
+    pass: "",
+  });
+  const [errorMsg, setErrorMsg] = useState("");
+  const [submitButtonDisabled, setSubmitButtonDisabled] = useState(false);
+
+  const handleSubmission = () => {
+    if (!values.email || !values.pass) {
+      setErrorMsg("Fill all fields");
+      return;
+    }
+    setErrorMsg("");
+
+    setSubmitButtonDisabled(true);
+    signInWithEmailAndPassword(auth, values.email, values.pass)
+      .then(async (res) => {
+        setSubmitButtonDisabled(false);
+        setFile("");
+        setGeneratedText("");
+        setSummarizedText("");
+        window.location.href = "/dashboard";
+      })
+      .catch((err) => {
+        setSubmitButtonDisabled(false);
+        setErrorMsg(err.message);
+      });
+  };
+  return (
+    <div className={styles.container}>
+      <div className={styles.headSection}>
+        <div>
+          <img src={process.env.PUBLIC_URL + `/logo.png`} alt="Logo" />
+        </div>
+        <div>
+          <h1 className="text-white">Quickey</h1>
+        </div>
+        <div></div>
+      </div>
+      <div className={styles.innerBox}>
+        <h1 className={"fw-bold"}>Login</h1>
+
+        <InputControl
+          label="Email"
+          onChange={(event) =>
+            setValues((prev) => ({ ...prev, email: event.target.value }))
+          }
+          placeholder="Enter email address"
+        />
+        <InputControl
+          label="Password"
+          type="password"
+          onChange={(event) =>
+            setValues((prev) => ({ ...prev, pass: event.target.value }))
+          }
+          placeholder="Enter Password"
+        />
+
+        <div className={styles.footer}>
+          <b className={styles.error}>{errorMsg}</b>
+          <button disabled={submitButtonDisabled} onClick={handleSubmission}>
+            Login
+          </button>
+          <p>
+            Already have an account?{" "}
+            <span>
+              <Link to="/signup">Sign up</Link>
+            </span>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
