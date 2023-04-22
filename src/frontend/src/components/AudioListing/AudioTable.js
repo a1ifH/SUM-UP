fc3ed150763ce284478c0f572ca23b88de24fbf8
Alif Hossain <alif.hossain5@mail.dcu.ie>
2023-04-22 10:40:12 +0000

    Updating and finishing AudioTable.js file

--- src/frontend/src/components/AudioListing/AudioTable.js
+++ src/frontend/src/components/AudioListing/AudioTable.js
@@ -12,6 +12,7 @@ const items = [
     label: (
       <div
         onClick={() => {
+          //   console.log(record);
         }}
       >
         View Details
@@ -38,6 +39,8 @@ const AudioTable = () => {
     } else {
       try {
         setLoading(true);
+        // Get the currently logged in user
+        // Get a reference to the subcollection that contains the user's data
         const audioToTextCollectionRef = collection(db, "audioToText");
         const audioToTextDataRef = doc(audioToTextCollectionRef, uid);
         const audioToTextSubCollectionRef = collection(
@@ -45,11 +48,14 @@ const AudioTable = () => {
           "records"
         );
 
+        // Query the subcollection to get all documents that belong to the user
         const audioToTextQuery = query(
           audioToTextSubCollectionRef,
           where("uid", "==", uid)
         );
         const audioToTextDocs = await getDocs(audioToTextQuery);
+
+        // Log the data for each document to the console
 
         audioToTextDocs.forEach((doc) => {
           setAudioRecord((audioRecord) => [...audioRecord, { ...doc.data() }]);
@@ -70,3 +76,78 @@ const AudioTable = () => {
       }
     }
   };
+  const onHide = () => {
+    setOpen(false);
+  };
+  const columns = [
+    {
+      title: "User Id",
+      dataIndex: "uid",
+      //   key: "uid",
+    },
+    {
+      title: "User Email",
+      dataIndex: "email",
+      //   key: "email",
+    },
+    {
+      title: "Generated Text",
+      dataIndex: "generatedText",
+      render: (_, record) => {
+        return <>{record.generatedText.slice(0, 50)} ...</>;
+      },
+    },
+    {
+      title: "Summarize Text",
+      dataIndex: "summarizedText",
+      render: (_, record) => {
+        return <>{record.summarizedText.slice(0, 50)} ...</>;
+      },
+    },
+    {
+      title: "Action",
+      // dataIndex: "address",
+      // key: "address",
+      render: (_, record) => {
+        return (
+          <>
+            <Dropdown
+              menu={{
+                items,
+                onClick: () => {
+                  setRecord(record);
+                  setOpen(true);
+                },
+              }}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-three-dots-vertical"
+                viewBox="0 0 16 16"
+              >
+                <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z" />
+              </svg>
+            </Dropdown>
+          </>
+        );
+      },
+    },
+  ];
+  return (
+    <>
+      {open && <AudioDetailModal open={open} onHide={onHide} record={record} />}
+      <Spin spinning={loading}>
+        <Table
+          style={{ overflowX: "scroll" }}
+          dataSource={audioRecord}
+          columns={columns}
+        />
+      </Spin>
+    </>
+  );
+};
+
+export default AudioTable;
